af2287557ad96a43dea1281033d16f57e4b1288a
Kiritouse <2496305505@qq.com>
2024-03-23 21:54:18 +0800

    目前的问题是不能使用非main名称的函数作为起始函数,并且调试的时候无法跳出到汇编中

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -3,7 +3,7 @@ int magic = OHMYOS_MAGIC;
 char message[] = "hello,ohmyos!!!"; //.data段中
 char buf[1024]; //.bss段中
 void kernel_init(){
-        char *video=(char *)0xb8000; //文本显示器的内存位置,只有操作系统才能访问
+        char *video=(char *) 0xb8000; //文本显示器的内存位置,只有操作系统才能访问
         for(int i = 0;i<sizeof(message);i++){
             video[i*2]=message[i];         
         }
